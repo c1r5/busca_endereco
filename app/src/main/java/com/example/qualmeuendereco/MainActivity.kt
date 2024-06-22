4f62a1a928efbe9b9a2ada5e8fdf8e00c5195bae
cirex9 <eric.joseph@sitx9.com.br>
2024-06-22 08:35:11 -0300

    implemented cep search feature into layout

--- app/src/main/java/com/example/qualmeuendereco/MainActivity.kt
+++ app/src/main/java/com/example/qualmeuendereco/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.example.qualmeuendereco
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.qualmeuendereco.databinding.ActivityMainBinding
 import com.example.qualmeuendereco.viewmodels.MainViewModel
+import com.google.android.material.textfield.TextInputEditText
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 
@@ -16,38 +15,46 @@ class MainActivity : AppCompatActivity() {
     private val activityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private val mainViewModel by viewModels<MainViewModel>()
 
-    private lateinit var cepField: EditText
+    private lateinit var cepField: TextInputEditText
     private lateinit var searchButton: Button
+    private lateinit var referenceField: TextInputEditText
+    private lateinit var ufField: TextInputEditText
+    private lateinit var cityField: TextInputEditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activityMainBinding.root)
 
         searchButton = activityMainBinding.searchButton
         cepField = activityMainBinding.cepField
+        referenceField = activityMainBinding.referenceField
+        ufField = activityMainBinding.ufField
+        cityField = activityMainBinding.cityField
 
         searchButton.setOnClickListener {
             val cep = cepField.text.toString()
             if (cep.isNotEmpty()) {
                 mainViewModel.consultaCep(cep)
+            } else {
+                Toast.makeText(this, "Digite um CEP válido", Toast.LENGTH_SHORT).show()
             }
         }
 
         MainScope().launch {
             mainViewModel.cep.collect {
                 it?.let { endereco ->
-                    Log.d("Endereco", endereco.toString())
-                } ?: Toast.makeText(this@MainActivity, "CEP não encontrado", Toast.LENGTH_SHORT)
-                    .show()
+                    referenceField.setText(endereco.bairro)
+                    ufField.setText(endereco.uf)
+                    cityField.setText(endereco.localidade)
+                }
             }
         }
 
         MainScope().launch {
             mainViewModel.error.collect {
-                Toast.makeText(this@MainActivity, "Erro ao consultar CEP", Toast.LENGTH_SHORT)
-                    .show()
-
                 it?.let { error ->
-                    Log.e("Error", error.message.toString())
+                    Toast.makeText(this@MainActivity, "Erro ao consultar CEP", Toast.LENGTH_SHORT)
+                        .show()
                 }
             }
         }
