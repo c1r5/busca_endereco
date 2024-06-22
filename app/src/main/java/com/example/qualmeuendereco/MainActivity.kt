f5eee54c557eee9dcda99d974fafca543fb4387f
cirex9 <eric.joseph@sitx9.com.br>
2024-06-21 22:23:29 -0300

    first commit

---
+++ app/src/main/java/com/example/qualmeuendereco/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.qualmeuendereco
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.qualmeuendereco.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val activityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activityMainBinding.root)
+    }
+}
